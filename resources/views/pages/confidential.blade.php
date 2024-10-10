6d583336ffdf5aedb0892d589c9ba4864bd722a8
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-10-10 16:29:52 +0600

    confidentialité page check

--- resources/views/pages/confidential.blade.php
+++ resources/views/pages/confidential.blade.php
@@ -64,16 +64,16 @@
             <li>les constructeurs.</li>
         </ul>
         <h3 class="fs-22 fw-500">
-            (i) Référencement des Annonceurs :
+            (ii) Référencement des Annonceurs :
         </h3>
         <p class="margin-top-18">
             Pour être référencée sur le Site et donc diffusée, une Annonce doit impérativement respecter les règles
             décrites ci-dessous.
         </p>
-        <h3 class="fs-22 fw-500 margin-top-43 margin-left-40 ">
+        <h3 class="fs-22 fw-500 margin-top-24 margin-left-40 ">
             <u>Contenu de l’Annonce</u>
         </h3>
-        <p class="margin-top-18">Le bien doit être diffusé dans sa catégorie et être conforme aux produits de diffusion
+        <p class="margin-top-26">Le bien doit être diffusé dans sa catégorie et être conforme aux produits de diffusion
             souscrits par l’Annonceur. Sur le Site, l’Annonce ne peut porter que sur :</p>
         <div class="margin-left-26">
             <p style="margin-bottom: -.2rem;">- la vente et la location de biens anciens, ayant déjà fait l’objet d’un
@@ -84,12 +84,95 @@
         <a class="fs-22 fw-500 text-primary ">
             https://support.myselogerpro.com/hc/fr/articles/360006057939-Charte-SeLoger
         </a>
-        <h3 class="fs-22 fw-500 margin-top-43 margin-left-40 ">
+        <h3 class="fs-22 fw-500 margin-top-34 margin-left-40 ">
             <u>Contenu interdit au sein d’une Annonce</u>
         </h3>
-        <p class="margin-top-18">
-            Toute Annonce contenant des éléments qui sembleraient contraires aux dispositions légales ou réglementaires, aux bonnes mœurs, aux règles de diffusion de notre Site ou susceptible de heurter les utilisateurs sera immédiatement refusée par la société DCF, donc non référencée sur le Site.
+        <p class="margin-top-28">
+            Toute Annonce contenant des éléments qui sembleraient contraires aux dispositions légales ou réglementaires,
+            aux bonnes mœurs, aux règles de diffusion de notre Site ou susceptible de heurter les utilisateurs sera
+            immédiatement refusée par la société DCF, donc non référencée sur le Site.
         </p>
-        
+        <p class="margin-top-19">Est notamment strictement interdit :</p>
+        <ul>
+            <li>
+                Tout contenu rédigé en langue étrangère comportant des termes ou des descriptions sans lien avec le
+                contenu proposé ;
+            </li>
+            <li>
+                Tout contenu comportant des termes ou des descriptions sans lien avec le contenu proposé ;
+            </li>
+            <li>
+                Tout contenu portant sur un bien immobilier ou programme de construction fictif ;
+            </li>
+            <li>
+                Tout contenu frauduleux, ou visant à tromper l’Utilisateur ;
+            </li>
+            <li>
+                Tout contenu à caractère politique, religieux ou haineux ;
+            </li>
+            <li>Tout contenu à caractère promotionnel et/ou publicitaire ;</li>
+            <li>Toute image ou photographie sans lien avec l’offre proposée, non autorisée, contrefaisante, ou encore à
+                caractère pornographique.</li>
+        </ul>
+        <h3 class="fs-22 fw-500 margin-top-36">
+            (iii) Déréférencement des Annonces et Annonceurs :
+        </h3>
+        <p class="margin-top-18">En cas de non-respect par l’Annonceur de ses engagements contractuels et / ou de la
+            réglementation en vigueur
+            et en particulier celle applicable aux Annonceurs professionnels de l’immobilier et/ou des règles de
+            référencement décrites ci-dessus et de la Charte de diffusion accessible ici, la société DCF se réserve le
+            droit, de refuser ou de bloquer la publication d’Annonces et / ou de suspendre le compte ou de résilier le
+            contrat de l’Annonceur, sans notification préalable le cas échéant.</p>
+
+        <p class="margin-top-18">La suppression puis la création, à l’identique, des mêmes Annonces immobilières est
+            strictement interdite. La
+            société DCF se réserve ainsi le droit de les déréférencer et/ou de suspendre le compte ou de résilier le
+            contrat
+            de l’Annonceur en cas de réitération.</p>
+
+        <p class="margin-top-18">De même, dans le cas où la société DCF, en sa qualité d’hébergeur de l’Annonce, devait
+            être rendue destinataire
+            d’un signalement ou d’une notification émanant d’une autorité nationale ou européenne, lui faisant part du
+            caractère manifestement illicite et/ou frauduleux de l’Annonce, celle-ci se réserve le droit, sans
+            notification
+            préalable, de bloquer la publication de l’Annonce ou de suspendre ou de supprimer le compte de l’Annonceur
+            et de
+            résilier son contrat.</p>
+
+        <p class="margin-top-18">Il en est notamment ainsi dans l’hypothèse où l’Annonceur (i) ne disposerait pas des
+            droits de propriété
+            intellectuelle sur le contenu de l’Annonce et/ou (ii) ne disposerait pas de la propriété et/ou jouissance du
+            bien et/ou le cas échéant, des autorisations requises, en particulier du propriétaire du bien, en vue de la
+            diffusion de l’Annonce présentant le bien.</p>
+
+        <h3 class="fs-22 fw-500 margin-top-36">
+            (iv) Signalement de contenu illégal :
+        </h3>
+        <p class="margin-top-18">DCF permet aux Utilisateurs de son Site de signaler tout contenu illégal, telle qu’une
+            arnaque, une tentative de fraude, un comportement suspect, une contrefaçon ou encore toute infraction aux
+            dispositions légales et réglementaires. Ces signalements peuvent être envoyés directement à la société DCF
+            depuis la page de l’Annonce ou de l’Annonceur.</p>
+
+        <h2 class="fw-bold h5 my-5">
+            2. CLASSEMENT DES ANNONCES :
+        </h2>
+        <h3 class="fs-22 fw-500 margin-top-36">
+            (i) Référencement des Annonceurs :
+        </h3>
+        <p class="margin-top-18">La diffusion d’annonces sur le Site est réservée aux Annonceurs définis ci-dessous :</p>
+        <p class="margin-top-18">(i) Annonceurs professionnels ayant souscrit à titre payant un contrat de diffusion
+            d’annonces de vente de biens immobiliers avec la société DCF et entrant dans l’une des catégories suivantes
+            :</p>
+        <ul>
+            <li>
+                les professionnels de l’immobilier détenant une carte de transaction ou de gestion conformément à la loi Hoguet n° 70-9 du 2 janvier 1970,
+            </li>
+            <li>les notaires,</li>
+            <li>les avocats exerçant à titre accessoire l’activité de mandataire en transactions immobilières,</li>
+            <li>les huissiers,</li>
+            <li>les promoteurs immobiliers,</li>
+            <li>les constructeurs.</li>
+            
+        </ul>
     </section>
 </x-app>
