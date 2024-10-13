9425ae2680d90289ca43d0894c99c3526c293887
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-10-13 13:18:49 +0600

    all user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ExtraSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(PropertiesSeeder::class);
 
     }
