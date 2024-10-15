84419dda1b847226ba8e247f815e09141199a31c
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2024-10-15 14:50:14 +0600

    done

---
+++ resources/views/pages/agency.blade.php
@@ -0,0 +1,27 @@
+<x-app>
+    <section class="container-fluid" id="agency-heading">
+        <img src="{{asset('assets/agency.png')}}" class="agency-cover" alt="">
+        <div class="agency-card">
+            <div class="row  h-100">
+                
+                <div class="middle col-md-2" >
+                    <img src="{{asset('assets/companyLogo.png')}}" class="agency-logo" alt="">
+                </div>
+                <div class="middle flex-column align-items-start col-md-3">
+                    <h3>
+                        RE/MAX Ra'annna
+                    </h3>
+                    <p>
+                        152 Properties • Member since 2023
+                    </p>
+                    <a href="">
+                        Ahuza St 70. Ramana
+                    </a>
+                </div>
+                <div class="col-md-3">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/listing', 'pages.listing');
+Route::view('/agency', 'pages.agency');
 Route::view('/confidential', 'pages.confidential');
 
 Route::view('mansion', 'pages.mansion');
